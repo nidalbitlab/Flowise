8da84a9380602e1733361c0c56aacb5827ea2467
nidal <nidal.banna@gmail.com>
2025-11-04 18:40:10 +0200

    fix

--- packages/api-documentation/src/index.ts
+++ packages/api-documentation/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, type RequestHandler } from 'express'
 import swaggerUi from 'swagger-ui-express'
 import { swaggerDocs, swaggerExplorerOptions } from './configs/swagger.config'
 
@@ -9,7 +9,10 @@ app.get('/', (req: Request, res: Response) => {
     res.redirect('/api-docs')
 })
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerExplorerOptions))
+// Ensure types align with Express typings in TS: spread serve array and cast setup to RequestHandler
+const swaggerServe = swaggerUi.serve as unknown as RequestHandler[]
+const swaggerSetup = swaggerUi.setup(swaggerDocs, swaggerExplorerOptions) as unknown as RequestHandler
+app.use('/api-docs', ...swaggerServe, swaggerSetup)
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
